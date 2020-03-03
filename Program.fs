72e6e6c5382bf5f0b2878e90db9203bc2ed75d41
Brian Liao <phyred23@gmail.com>
2020-03-03 12:25:18 -0800

    init commit, it kind of works!!!!

---
+++ Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open Python.Runtime
+open FSharp.Interop.Dynamic
+open System.Collections.Generic
+open System
+
+[<EntryPoint>]
+let main argv =
+    use gil = Py.GIL()
+
+    let np = Py.Import("numpy")
+
+    let sinResult = np?sin(5)
+
+    Py.Print sinResult
+
+    // printfn "%d" sinResult
+    0 // return an integer exit code
